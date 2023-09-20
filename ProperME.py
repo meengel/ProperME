c8b60e634e8ab524b41f9d9f0a39387610c70618
meengel <m-engel@outlook.com>
2023-09-21 00:18:54 +0200

    corrected computegraph for setters

--- ProperME.py
+++ ProperME.py
@@ -8,11 +8,11 @@ class ProperME:
         
         self._depending = dependingdict
         
-        self._has_children = self._build_has_childrendict(self._depending) # determine whether any other method depends on key-method
+        self._has_children = self._get_has_childrendict(self._depending) # determine whether any other method depends on key-method
         self._dependent = {key:[] for key in self._depending.keys()}
         for key in self._dependent.keys():
             # recursive dependency graph calculation for key-method i.e. which methods are depending on key-method
-            self._build_dependentdict(
+            self._get_dependentdict(
                 key = key,
                 dependentlist = self._dependent[key],
                 visited = {key:False for key in self._depending.keys()}
@@ -31,7 +31,7 @@ class ProperME:
         
             if "get"==mode: # for getters
                 def getter(*args,**kwargs):
-                    _computegraph = self._build_computegraph(args[0]) # load computegraph from obj -> important for pickle
+                    _computegraph = self._get_computegraph(args[0]) # load computegraph from obj -> important for pickle
                         
                     if not hasattr(args[0], varname) or _computegraph[funname]: # check if already computed or to be computed
                         res = fun(*args,**kwargs) # compute method
@@ -43,10 +43,11 @@ class ProperME:
             
             else: # for setters
                 def setter(*args,**kwargs):
-                    _computegraph = self._build_computegraph(args[0]) # load computegraph from obj -> important for pickle
+                    _computegraph = self._get_computegraph(args[0]) # load computegraph from obj -> important for pickle
                     # mark all dependent methods as to-compute
                     for key in self._dependent["get"+varname]:
                         _computegraph[key] = True # mark method as to-compute
+                    _computegraph["get"+varname] = False # mark corresponding get-method as done as being set by setter
                     return fun(*args,**kwargs)
                 return setter   
             
@@ -54,20 +55,20 @@ class ProperME:
             return fun
     
     @staticmethod
-    def _build_has_childrendict(dependingdict):
+    def _get_has_childrendict(dependingdict):
         has_children_dict = {}
         for key in dependingdict.keys():
             has_children_dict[key] = np.any([True if key in dependingdict[key2] else False for key2 in dependingdict.keys()])
         return has_children_dict
     
-    def _build_dependentdict(self, key, dependentlist, visited):
+    def _get_dependentdict(self, key, dependentlist, visited):
         dependentlist.extend(list(set([key2 if key in self._depending[key2] else key for key2 in self._depending.keys()])))
         visited[key] = True
         for key_ in dependentlist:
             if self._has_children[key_] and not visited[key_]:
-                self._build_dependentdict(key_, dependentlist, visited)
+                self._get_dependentdict(key_, dependentlist, visited)
     
-    def _build_computegraph(self, obj):
+    def _get_computegraph(self, obj):
         _computegraph = getattr(obj, self._computegraphname)
         if not _computegraph:
             setattr(obj, self._computegraphname, {key:True for key in self._dependent.keys()})
